50be8f4928e950b37e185f96401363cf4904d702
raja990832 <66311018+raja990832@users.noreply.github.com>
2020-07-10 17:37:50 +0530

    Update V2__insert_table.sql

--- src/main/resources/db/migration/V2__insert_table.sql
+++ src/main/resources/db/migration/V2__insert_table.sql
@@ -1,4 +1,4 @@
         INSERT INTO person (id) VALUES(1);
         INSERT INTO person (id) VALUES(2);
         INSERT INTO person (id) VALUES(3);
-        INSERT INTO person (id) VALUES(4);
+      
